b7c49d7f38b1fb8fdff176f56c6adb995aeaaa23
darknight <illuminating.me@gmail.com>
2019-03-24 12:28:24 +0800

    practise mutex

--- src/concurrency/mod.rs
+++ src/concurrency/mod.rs
@@ -1,3 +1,4 @@
 mod dead_lock;
 mod thread_pool;
 mod atomic;
+mod two_threads;
---
+++ src/concurrency/two_threads.rs
@@ -0,0 +1,61 @@
+///
+/// Two threads A & B, A prints 1, 3, 5..., B prints 2, 4, 6...
+/// Console output: 1, 2, 3, 4...
+///
+use std::thread;
+use std::sync::Mutex;
+use std::sync::Arc;
+use std::time::Duration;
+
+// use shared state
+fn thread_print_by_mutex() {
+
+    let mutex = Arc::new(Mutex::new(1u32));
+
+    let odd_mutex = Arc::clone(&mutex);
+    let odd_thread = thread::spawn(move || {
+        loop {
+            thread::sleep(Duration::from_millis(100));
+            let mut counter = odd_mutex.lock().unwrap();
+            if *counter & 0x00000001 == 1 {
+                println!("{}", *counter);
+                *counter += 1;
+            }
+        }
+    });
+
+    let even_mutex = Arc::clone(&mutex);
+    let even_thread = thread::spawn(move || {
+        loop {
+            thread::sleep(Duration::from_millis(100));
+            let mut counter = even_mutex.lock().unwrap();
+            if *counter & 0x00000001 == 0 {
+                println!("{}", *counter);
+                *counter += 1;
+            }
+        }
+    });
+
+    odd_thread.join();
+    even_thread.join();
+}
+
+// TODO: use message passing
+fn thread_print_by_channel() {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        thread_print_by_mutex()
+    }
+
+    #[test]
+    fn test2() {
+        thread_print_by_mutex()
+    }
+}
